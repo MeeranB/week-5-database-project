c81e03d8ba3ce8d5fc0c2ea362800e2114b366f0
Shivani Shah <shivanishah96@hotmail.co.uk>
2020-07-28 11:47:48 +0100

    add functionality comments

--- src/handlers/outputPosts.js
+++ src/handlers/outputPosts.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const dbConnection = require("../../database/db_connection");
 
 function outputPostsHandler(request, response) {
-  //create a query that joins the users and posts together
+  //create a query that joins the users and posts tables together. try inner right and left?
   //list out the queries - possibly using map to create a new array with the fields of username and post ?
   dbConnection
     .query(
@@ -25,9 +25,19 @@ function outputPostsHandler(request, response) {
         </li>
       `
       );
+      //console.log(postsList);
+
       response.writeHead(200, { "content-type": "text/html" });
       response.end(`<ul>${postsList.join("")}</ul>`);
     });
 }
 
 module.exports = outputPostsHandler;
+
+//if there is a user that areasy exists in the table  do not add to the user table
+// else we do add it to the user table
+// add to the posts table with the user_id (from the user table)
+//in the instance where it is a non matching keyword_id we want to highlight the post
+// we will do this when displaying the posts by checking each posts user_id in the user table
+//and seeing if the current posts has a matching keyword_id when the respective users keyword-id
+//if they do not match then highlight the post otherwise display it normally
--- src/handlers/test.js
+++ src/handlers/test.js
@@ -5,7 +5,7 @@ const dbConnection = require("../../database/db_connection");
 function testHandler(request, response) {
   console.log("test handler reached");
   let body = "";
-  request.on("data", chunk => {
+  request.on("data", (chunk) => {
     body += chunk;
     console.log(chunk);
   });
@@ -14,21 +14,21 @@ function testHandler(request, response) {
     const data = new URLSearchParams(body);
     console.log(data);
     const form = Object.fromEntries(data);
-    const values = [form.userInput, form.keyInput]
-    dbConnection.query(`INSERT INTO users (username, keyword_id) VALUES ($1, $2)`, values)
-    .then(res =>
-      dbConnection.query(`SELECT * FROM users`)
-      .then(res => {
-        const userTable = JSON.stringify(res.rows);
-        console.log(userTable);
-        response.writeHead(200, {'content-type': 'application/json'});
-        response.end(userTable);
-      })
-    )
-    .catch(err => {
-      console.log(err);
-    })
- })
+    const values = [form.userInput, form.keyInput];
+    dbConnection
+      .query(`INSERT INTO users (username, keyword_id) VALUES ($1, $2)`, values)
+      .then((res) =>
+        dbConnection.query(`SELECT * FROM users`).then((res) => {
+          const userTable = JSON.stringify(res.rows);
+          console.log(userTable);
+          response.writeHead(200, { "content-type": "application/json" });
+          response.end(userTable);
+        })
+      )
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 }
 
 module.exports = testHandler;
@@ -41,7 +41,7 @@ module.exports = testHandler;
 //     console.log(res);
 //   });
 // }
-  
+
 // function postData(cb, user) {
 //     dbConnection.query(`INSERT INTO users (username) VALUES (${user})`, (err, res) => {
 //       if (err) return cb(err);
@@ -50,3 +50,11 @@ module.exports = testHandler;
 //       cb(null, res.rows);
 //     });
 // };
+
+//if there is a user that areasy exists in the table  do not add to the user table
+// else we do add it to the user table
+// add to the posts table with the user_id (from the user table)
+//in the instance where it is a non matching keyword_id we want to highlight the post
+// we will do this when displaying the posts by checking each posts user_id in the user table
+//and seeing if the current posts has a matching keyword_id when the respective users keyword-id
+//if they do not match then highlight the post otherwise display it normally
