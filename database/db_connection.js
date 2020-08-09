96f2920ccec4b8b911244254886e2a191e5a38e3
Meeran B <v9@live.co.uk>
2020-08-09 20:32:35 +0100

    remote database connection fix

--- database/db_connection.js
+++ database/db_connection.js
@@ -7,20 +7,22 @@ console.log(process.env.DATABASE_URL);
 if (!process.env.DATABASE_URL)
   throw new Error("Environment variable DATABASE_URL must be set");
 
-// const params = url.parse(process.env.DATABASE_URL);
-// const [username, password] = params.auth.split(":");
+const params = url.parse(process.env.DATABASE_URL);
+const [username, password] = params.auth.split(":");
 
-// const options = {
-//   host: params.hostname,
-//   port: params.port,
-//   database: params.pathname.split("/")[1],
-//   max: process.env.DB_MAX_CONNECTIONS || 2,
-//   user: username,
-//   password,
-//   // ssl: params.hostname !== "localhost",
-//   ssl: { rejectUnauthorized: false },
-// };
+const options = {
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split("/")[1],
+  max: process.env.DB_MAX_CONNECTIONS || 2,
+  user: username,
+  password,
+  // ssl: params.hostname !== "localhost",
+  ssl: { rejectUnauthorized: false },
+};
 
-module.exports = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+module.exports = new Pool(options);
+
+// module.exports = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+// });
