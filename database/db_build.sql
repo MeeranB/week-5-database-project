e5e4f43839edc01d0253682dd7c900b5c12f9d4f
Meeran B <v9@live.co.uk>
2020-08-09 12:00:10 +0100

    add relevant not null constraints to db

--- database/db_build.sql
+++ database/db_build.sql
@@ -4,20 +4,20 @@ DROP TABLE IF EXISTS users, posts, keywords CASCADE;
 
 CREATE TABLE keywords (
     id SERIAL PRIMARY KEY,
-    keyword_name VARCHAR(50)
+    keyword_name VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(20) NOT NULL UNIQUE,
-    keyword_id INTEGER REFERENCES keywords(id)
+    keyword_id INTEGER NOT NULL REFERENCES keywords(id)
 );
 
 CREATE TABLE posts (
     id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES users(id),
-    posted_with_keyword INTEGER REFERENCES keywords(id),
-    text_content VARCHAR(255),
+    posted_with_keyword INTEGER NOT NULL REFERENCES keywords(id),
+    text_content VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     
 );
