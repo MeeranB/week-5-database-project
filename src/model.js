8b634e9ca3e952b73d691ad24a3f95e6bf6af901
Meeran B <v9@live.co.uk>
2020-08-09 13:58:31 +0100

    add highlight implementation plans

--- src/model.js
+++ src/model.js
@@ -66,6 +66,18 @@ function checkUserID(searchParams) {
     });
 }
 
+function checkKeywordID(searchParams) {
+  const values = [searchParams.colorInput];
+  return dbConnection
+    .query(`SELECT id FROM keywords WHERE keyword_name = $1`, values)
+    .then((res) => {
+      return res.rows[0]["id"];
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
 function getPosts() {
   //Returns a string of all posts to be passed into the html
   return dbConnection
@@ -82,17 +94,24 @@ function getPosts() {
     });
 }
 
-function checkKeywordID(searchParams) {
-  const values = [searchParams.colorInput];
-  return dbConnection
-    .query(`SELECT id FROM keywords WHERE keyword_name = $1`, values)
-    .then((res) => {
-      return res.rows[0]["id"];
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}
+/* TODO: Highlighting process */
+//Get posts gets username, text_content, posted_with_keyword, created_at
+//In templates, for each object in get posts array:
+//Select username's keyword_id from users:
+//if this does not match posted_with_keyword map postsList to be highlighted
+//else map postList in current styling
+//return html
+
+/* TODO: Alternate better(?) Highlighting process */
+// This approach has significantly fewer sql queries, however requires an extra column to be created
+//Add new column to posts table which represents if highlight is true, using bit datatype
+//Get posts gets username, text_content, highlight and created_at
+//In templates, for each object in get posts array:
+//if highlight property is true, map to highlighted html string
+//else map to current html string
+//return HTML
+
+
 
 module.exports = {
   newUser,
